24ebd23cbb42a284c838e4ec2ac37338f9eefd58
KWaloen <k.waloen91@hotmail.com>
2025-01-04 17:33:44 +0100

    Api fetch

---
+++ Finished/apiFinished.py
@@ -0,0 +1,10 @@
+import requests
+import base64
+
+url = f"https://api.github.com/repos/tc39/proposals/contents/finished-proposals.md"
+
+response = requests.get(url)
+data = response.json()
+file_content = base64.b64decode(data["content"]).decode("utf-8")
+
+print(file_content)
