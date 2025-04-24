1a10d3037fbab4d745f7fd4c3831e785f7cc99c9
KWaloen <k.waloen91@hotmail.com>
2025-04-24 13:42:31 +0200

    hacky solution to get tags and dates

--- sharedMethods/getDataForScatterplot.py
+++ sharedMethods/getDataForScatterplot.py
@@ -13,6 +13,7 @@ import sys
 import os
 import re
 import csv
+import datetime
 
 
 def extractBumpsAndCommit(title, completePath):
@@ -314,13 +315,113 @@ def getStageSpecificClassifiedChanges(change, stage):
         return
 
 
+###################################################################################
+#
+#Extract tags by:
+#python sharedMethods/getDataForScatterplot.py tags
+#
+###################################################################################
+
+def extractTagStage1Stage4(fullPath):
+    extract = []
+    tags = []
+    stage1 = datetime.datetime.now
+    stage4 = datetime.datetime.now
+    with open(f"{fullPath}", "r") as proposal:
+        proposalContent = proposal.read()
+        for line in proposalContent.split("<br>"):
+            if "Keywords:" in line:
+                tags = re.findall(r'#[\w-]+', line)
+            if "Stage 1:" in line:
+                stage1 = re.search(r"Stage 1:\s+([^\s<]+)", line)
+            if "Stage 4:" in line:
+                stage4 = re.search(r"Stage 4:\s+([^\s<]+)", line)
+
+    for each in tags:
+        extract = [each, stage1.group(1).strip(), stage4.group(1).strip()]
+
+    return extract
+
+def readProposals(Stage):
+    tags = []
+    path = f"Obsidian_TC39_Proposals/Proposals/{Stage}/"
+    if os.path.isdir(path):
+        for proposal in os.listdir(path):
+            try:
+                title = proposal.split(".md")[0]
+                fullPath = os.path.join(path, proposal)
+                print(proposal)
+                with open(fullPath, "r") as proposal:
+                    content = proposal.read()
+                    if "[[API Change]]" in content and "[[Syntactic Change]]" in content and "[[Semantic Change]]" in content:
+                        tags.append(extractTagStage1Stage4(fullPath))
+                        
+                    if "[[API Change]]" in content and "[[Syntactic Change]]" in content and "[[Semantic Change]]" not in content:
+                        tags.append(extractTagStage1Stage4(fullPath))
+                       
+                    if "[[API Change]]" in content and "[[Semantic Change]]" in content and "[[Syntactic Change]]" not in content:
+                        tags.append(extractTagStage1Stage4(fullPath))
+                        
+                    if "[[Syntactic Change]]" in content and "[[Semantic Change]]" in content and "[[API Change]]" not in content:
+                        tags.append(extractTagStage1Stage4(fullPath))
+                        
+                    if "[[Syntactic Change]]" in content and "[[Semantic Change]]" not in content and "[[API Change]]" not in content:
+                        tags.append(extractTagStage1Stage4(fullPath))
+                        
+                    if "[[Semantic Change]]" in content and "[[Syntactic Change]]" not in content and "[[API Change]]" not in content:
+                        tags.append(extractTagStage1Stage4(fullPath))
+                       
+                    if "[[API Change]]" in content and "[[Semantic Change]]" not in content and "[[Syntactic Change]]" not in content:
+                        tags.append(extractTagStage1Stage4(fullPath))
+                                              
+            except Exception as e:
+                print(f"Error with: {fullPath} - {e}")
+
+    return tags
+            
+
+def getTags():
+    returnList = []
+    tagList = []
+    print("-------------------------------Extracting Tags-----------------------------------")
+
+    print("\n----------------------------Extracting Tags for Stage 4--------------------------")
+    tagList.append(readProposals("Stage 4"))
+
+    print("\n----------------------------Extracting Tags for Stage 3--------------------------")
+    tagList.append(readProposals("Stage 3"))
+
+    print("\n----------------------------Extracting Tags for Stage 2.7------------------------")
+    tagList.append(readProposals("Stage 2.7"))
+
+    print("\n----------------------------Extracting Tags for Stage 2--------------------------")
+    tagList.append(readProposals("Stage 2"))
+
+    print("\n----------------------------Extracting Tags for Stage 1--------------------------")
+    tagList.append(readProposals("Stage 1"))
+
+    print("\n----------------------------Extracting Tags for Stage 0--------------------------")
+    tagList.append(readProposals("Stage 0"))
+
+    print("\n----------------------------Extracting Tags for Inactive-------------------------")
+    tagList.append(readProposals("Inactive"))
+
+    for each in tagList:
+        for each in each:
+            returnList.append(each)
+
+    with open(f"Data Analysis/Tags/TagDates.csv", "w", newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(returnList)
 
     
 
 
 if __name__ == "__main__":
 
-    if len(sys.argv) == 3:
+    if sys.argv[1] == "tags":
+        getTags()
+    elif len(sys.argv) == 3:
         if sys.argv[1] == "stage":
             stage = sys.argv[2]
             getStageBumpsAndLastCommit(stage)
